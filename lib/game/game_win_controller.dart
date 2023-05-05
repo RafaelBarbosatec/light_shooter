a9d257340eca1ec74401cd56fed628fd6afb1f5e
rafaelbarbsoatec <rafaelbarbsoatec@gmail.com>
2023-05-05 14:13:08 -0300

    finalzia integracao

--- lib/game/game_win_controller.dart
+++ lib/game/game_win_controller.dart
@@ -4,10 +4,19 @@ import 'package:light_shooter/pages/home/home_route.dart';
 
 class GameWinController extends GameComponent {
   bool finishedGame = false;
+  bool checking = false;
+
+  @override
+  void onMount() {
+    Future.delayed(const Duration(seconds: 1), () {
+      checking = true;
+    });
+    super.onMount();
+  }
 
   @override
   void update(double dt) {
-    if (checkInterval('CHECK_WIN', 250, dt) && !finishedGame) {
+    if (checkInterval('CHECK_WIN', 250, dt) && !finishedGame && checking) {
       _verifyWinOrLose();
     }
     super.update(dt);
